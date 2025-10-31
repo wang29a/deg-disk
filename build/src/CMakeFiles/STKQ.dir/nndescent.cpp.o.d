d61c25c9bec743aef954f7c4f6998da02b2eb4bf
29a <2651419874@qq.com>
2025-10-31 16:46:51 +0800

    rm: remove build dir

--- build/src/CMakeFiles/STKQ.dir/nndescent.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/STKQ.dir/nndescent.cpp.o: \
- /mnt/hdd/yinziqi/yinziqi/DEG/src/nndescent.cpp \
- /usr/include/stdc-predef.h
--- build/src/CMakeFiles/STKQ_s.dir/nndescent.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/STKQ_s.dir/nndescent.cpp.o: \
- /mnt/hdd/yinziqi/yinziqi/DEG/src/nndescent.cpp \
- /usr/include/stdc-predef.h
